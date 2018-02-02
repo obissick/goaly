78da1a1035b5b834c98ef696b3092983deea4518
obissick <obissick@greathealthworks.com>
2018-02-02 18:27:39 -0500

    goals and comments model, controller and migration.

---
+++ database/migrations/2018_02_02_212542_create_goals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->string('user_id')->references('id')->on('users');
+            $table->integer('goal_type_id')->references('id')->on('goal_types');
+            $table->timestamp('target_date');
+            $table->timestamp('completed_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
